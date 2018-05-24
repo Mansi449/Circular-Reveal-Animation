ba8cdd7305e934b0c53db7325309c80b0df53151
Maanu449 <mansi10022000@gmail.com>
2018-05-25 00:07:40 +0530

    implemented the basic circular reveal animation

--- app/src/main/java/mdg/com/circularreveal/CardAdapter.java
+++ app/src/main/java/mdg/com/circularreveal/CardAdapter.java
@@ -1,11 +1,16 @@
 package mdg.com.circularreveal;
 
+import android.animation.Animator;
 import android.content.Context;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -18,6 +23,8 @@ public class CardAdapter extends android.support.v7.widget.RecyclerView.Adapter<
 
     private Context mContext;
     private ArrayList<ModelClass> textList = new ArrayList<>();
+    private LinearLayout cardParent;
+
 
     public class CardViewHolder extends RecyclerView.ViewHolder {
         public TextView card_text;
@@ -25,6 +32,7 @@ public class CardAdapter extends android.support.v7.widget.RecyclerView.Adapter<
         public CardViewHolder(View view) {
             super(view);
             card_text = (TextView) view.findViewById(R.id.card_text);
+            cardParent = (LinearLayout) view.findViewById(R.id.cardParent);
         }
     }
 
@@ -54,4 +62,21 @@ public class CardAdapter extends android.support.v7.widget.RecyclerView.Adapter<
         return textList.size();
     }
 
+    public void CircularRevealEffect(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            // get the center for the clipping circle
+            int cx = cardParent.getWidth() / 2;
+            int cy = cardParent.getHeight() / 2;
+
+            float finalRadius = (float) Math.hypot(cx, cy);
+            Animator anim =
+                    ViewAnimationUtils.createCircularReveal(cardParent, cx, cy, 0, finalRadius);
+
+            // make the view visible and start the animation
+            cardParent.setVisibility(View.VISIBLE);
+            anim.start();
+        } else {
+            // set the view to visible without a circular reveal animation below Lollipop
+            cardParent.setVisibility(View.VISIBLE);
+        }    }
 }
--- app/src/main/java/mdg/com/circularreveal/MainActivity.java
+++ app/src/main/java/mdg/com/circularreveal/MainActivity.java
@@ -1,9 +1,13 @@
 package mdg.com.circularreveal;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FloatingActionButton fab = findViewById(R.id.fab);
+
         //getting the recyclerview from xml
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -28,12 +34,15 @@ public class MainActivity extends AppCompatActivity {
         textList = new ArrayList<>();
 
         textList.add(new ModelClass("Card 1"));
-        textList.add(new ModelClass("Card 2"));
-        textList.add(new ModelClass("Card 3"));
-        textList.add(new ModelClass("Card 4"));
-        textList.add(new ModelClass("Card 5"));
 
         adapter = new CardAdapter(this, textList);
         recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.CircularRevealEffect();
+            }
+        });
     }
 }
